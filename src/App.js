de655bfedc80fc62aeb799feb862f90ee89161d4
Sudeep M Arashingodi <sudeepma99@gmail.com>
2023-03-10 19:24:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import Header from './comp/Header';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
+const [name,setname]=useState("");
+const [email,setemail]=useState("");
+const [data,setdata]=useState([]);
+
+
+function fas(){
+if(name!="" && email!="")
+ setdata([...data,{name:name,email:email}]);
+ setname("");
+ setemail("");
+}
+
+function deldata(ind){
+var arr=data;
+arr.splice(ind,1);
+setdata([...arr]);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header/>
+       <div className='indiv'>
+       <TextField value={name} onChange={(e)=>setname(e.target.value) } id="outlined-basic" label="Name" variant="outlined" 
+       inputProps={{style: {fontSize: 15}}}
+       InputLabelProps={{style: {fontSize: 18}}}
+       />
+       <TextField value={email}  onChange={(e)=>setemail(e.target.value) } id="outlined-basic" label="Email" variant="outlined" 
+        inputProps={{style: {fontSize: 18}}}
+       InputLabelProps={{style: {fontSize: 18}}}
+       />
+       <Button onClick={fas} variant="contained"><AddIcon /></Button>
+       </div>
+       <div className='datamain'>
+        { (()=>{
+          if(data.length>0)
+          {
+            return(<div className='heading'><h2>Name</h2>
+    <h2>Email</h2>
+    <h2>Delete</h2></div>);
+          }
+        })() }
+
+        {
+          data.map((ele,ind)=>{
+            return(
+              
+              <div key={ind} className='head'>
+    <h3>{ele.name}</h3>
+    <h3>{ele.email}</h3>
+    <Button onClick={()=>deldata(ind)} variant="outlined" color="error">
+    <DeleteIcon />
+     </Button>
+</div>
+            )
+          })
+        }
+       </div>
     </div>
   );
 }
